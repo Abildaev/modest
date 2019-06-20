95fc6b73694d4ddcddeca33e2ec637a4d5973840
dastan <abildaev1996@gmail.com>
2019-06-20 21:46:04 +0600

    added slider and finished project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$( function (){
+$(document).ready(function () {
 
 
      $('.header__hamburger_trigger').on('click', function (){
@@ -15,6 +15,47 @@ $( function (){
 
      });
 
+     var Slider = {
+          slideIndex: 1,      
+          showSlides: function(n = 1) {
+            var slides = $('.mySlides');
+            var dots = $('.dot');
+      
+            if(n < 1) {
+              this.slideIndex = slides.length;
+            }
+            if (n > slides.length) {
+              this.slideIndex = 1;
+            }
+      
+            slides.css('display', 'none');
+            dots.removeClass('active');
+      
+            $(slides[this.slideIndex - 1]).css('display', 'block');
+            $(dots[this.slideIndex - 1]).addClass('active');
+          },
+      
+          currentSlide: function () {
+            var dots = $('.dot');
+            var self = this;
+      
+            dots.on('click', function () {
+              self.showSlides(self.slideIndex = $(this).index() + 1);
+            });
+          },
+          play: function () {
+            setInterval( () => {
+              this.showSlides(this.slideIndex += 1);
+            }, 5000);
+          },
+          init: function () {
+            this.showSlides();
+            this.currentSlide();
+            this.play();
+          }
+        };
+      
+        Slider.init();
 });
 
 
